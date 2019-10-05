132bb847f8fa3b59ed3d914594886295932d43aa
R-Gene <rozilgene26@gmail.com>
2019-10-05 19:48:06 -0400

    third commit

---
+++ app/src/main/java/com/example/androidlabs/MainActivity2.java
@@ -0,0 +1,86 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity2 extends AppCompatActivity {
+     EditText mail;
+
+     public static final String D = "try";
+
+     SharedPreferences sharedPreferences;
+     private Button button;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+       mail=(EditText) findViewById(R.id.mail);
+
+
+
+
+
+        sharedPreferences=getSharedPreferences("data",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor= sharedPreferences.edit();
+
+        String email =sharedPreferences.getString("data","");
+        if(email.equals(D)){
+
+        }
+        else{
+            mail.setText(email);
+        }
+
+
+
+    }
+
+    protected void onPause(){
+
+        sharedPreferences= getSharedPreferences("data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("data",mail.getText().toString());
+        editor.commit();
+        super.onPause();
+
+
+    }
+
+
+   public void login(View v){
+
+       button =(Button) findViewById(R.id.Login);
+       button.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(MainActivity2.this, ProfileActivity.class );
+               EditText mail=(EditText)findViewById(R.id.mail);
+               String emailReal=mail.getText().toString();
+               intent.putExtra("EMAIL", emailReal);
+               startActivity(intent);
+           }
+
+
+       });
+    }
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,91 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.util.Log;
+public class ProfileActivity extends AppCompatActivity {
+
+    ImageButton mImageButton;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private TextView userEmail;
+
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+       Bundle intent = getIntent().getExtras();
+        String mailing=intent.getString("EMAIL");
+        TextView editText=(TextView) findViewById(R.id.email);
+        editText.setText(mailing);
+
+
+     /* userEmail= findViewById(R.id.email);
+      Intent intent = getIntent();
+      String email = intent.getStringExtra("EMAIL");
+      userEmail.setText(email);*/
+
+
+        Log.e(ACTIVITY_NAME,"onCreate()");
+        mImageButton=(ImageButton) findViewById(R.id.imagePic);
+
+
+    }
+
+    public void dispatchTakePictureIntent(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+    protected void onStart(){
+        Log.e(ACTIVITY_NAME,"onStart()");
+        super.onStart();
+    }
+
+    protected void onResume(){
+        Log.e(ACTIVITY_NAME,"onResume()");
+        super.onResume();
+    }
+    protected void onPause(){
+        Log.e(ACTIVITY_NAME,"onPause()");
+        super.onPause();
+    }
+    protected void onStop(){
+        Log.e(ACTIVITY_NAME,"onStop()");
+        super.onStop();
+    }
+    protected void onDestroy(){
+        Log.e(ACTIVITY_NAME,"onDestroy()");
+        super.onDestroy();
+    }
+
+
+
+
+
+
+
+
+
+
+}
